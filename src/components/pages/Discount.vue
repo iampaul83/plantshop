78adfc0ee4e5b7084ef2d2f5da5b37a21642ce9f
ping <qvod100203006@gmail.com>
2018-09-18 19:02:54 +0800

    upload

--- src/components/pages/Discount.vue
+++ src/components/pages/Discount.vue
@@ -4,7 +4,7 @@
       <div class="col-md-8">
       <h1 class="text-center mt-4 discount-title pb-3 "><span class="icon"><i class="fas fa-ticket-alt"></i> </span> 優惠活動</h1>
       <div class="activity-wrapper d-flex p-4 my-4">
-        <div class="activity-img"><img src="../../assets/images/alert.svg" alt=""></div>
+        <div class="activity-img"><img src="../../assets/images/alert.png" class="img-fluid" alt=""></div>
         <div class="activity-content d-flex justify-content-center flex-column">
           <h2 class="text-center text-danger py-2">開幕慶優惠 !!</h2>
           <div class="text-center font-weight-normal h5">輸入以下代碼，即可獲得全館7折優惠
